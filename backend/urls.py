f002f9efa5588c2f6dbbfa10d44eb7ea3da32b1a
Shuubb <shubitidzed9@gmail.com>
2023-11-24 14:54:29 +0400

    prepared for deployment

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('auth_api/', include('auth_api.urls')),
-    path('item_management/', include('item_management.urls')),
+    path('api/auth_api/', include('auth_api.urls')),
+    path('api/item_management/', include('item_management.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
